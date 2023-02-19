c0e62567d2ddb07b5e22f3adbe8f20ae7291f149
shingler <singlerwong@gmail.com>
2023-02-19 17:25:02 +0800

    新增对ini配置文件的支持

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,7 @@
+import os
+import sys
+from configparser import ConfigParser
+
 from flask import Flask
 
 import device
@@ -7,8 +11,25 @@ import log
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object("settings.Development")
-    # app.config.from_object("settings.Test")
+    # 根据环境读取不同变量
+    env_file = os.path.join(os.getcwd(), "..", "..", "env.ini")
+    # print(env_file)
+    if not os.path.exists(env_file):
+        sys.exit("环境文件不存在")
+
+    conf = ConfigParser()
+    conf.read(env_file, encoding="utf-8")
+    if not conf.has_option("env", "env"):
+        sys.exit("环境文件格式有误")
+
+    if conf["env"]["env"].lower() == "development":
+        app.config.from_object("settings.Development")
+    elif conf["env"]["env"].lower() == "test":
+        app.config.from_object("settings.Test")
+    else:
+        app.config.from_object("settings.Product")
+
+    # 注册蓝图
     app.register_blueprint(member.blueprint)
     app.register_blueprint(device.blueprint)
     app.register_blueprint(log.blueprint)
--- tests/conftest.py
+++ tests/conftest.py
@@ -3,13 +3,30 @@
 import pytest
 import requests
 from flask import Flask
+from configparser import ConfigParser
+import os
 
 
 @pytest.fixture(scope="session")
 def init_app():
     app = Flask(__name__)
-    app.config.from_object("settings.Development")
-    # app.config.from_object("settings.Test")
+    # 根据环境读取不同变量
+    env_file = os.path.join(os.getcwd(), "..", "..", "env.ini")
+    # print(env_file)
+    if not os.path.exists(env_file):
+        pytest.exit("环境文件不存在", returncode=1)
+
+    conf = ConfigParser()
+    conf.read(env_file, encoding="utf-8")
+    if not conf.has_option("env", "env"):
+        pytest.exit("环境文件格式有误", returncode=1)
+
+    if conf["env"]["env"].lower() == "development":
+        app.config.from_object("settings.Development")
+    elif conf["env"]["env"].lower() == "test":
+        app.config.from_object("settings.Test")
+    else:
+        app.config.from_object("settings.Product")
 
     app_context = app.app_context()
     app_context.push()
